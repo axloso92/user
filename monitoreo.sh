cddb4489173a00e5db159d29677dd972db2462a6
SkyllerXBruce <skyllerx@gmail.com>
2017-07-27 16:00:18 -0500

    creación y modificacion

---
+++ monitoreo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PROCESOS=`ps -fea|wc -l`
+FICH=/home/bruce/Escritorio/Lab_Super_Computo/Repositorios/README.md
+echo -e "# Super Computo\n####Estado:\n\n~~~~~\nSe ejecutaron $PROCESOS procesos\n~~~~~"  > $FICH
+
+git add *
+git commit -m "creación y modificacion"
+
