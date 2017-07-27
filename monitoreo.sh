ab708735fa357c7083dc1400d6f87e3fa51718d2
SkyllerXBruce <skyllerx@gmail.com>
2017-07-27 04:18:27 -0500

    Modificación 6

--- monitoreo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-procesos=`ps -fea|wc -l`
-fich=/home/bruce/Escritorio/Lab_Super_Computo/Repositorios/user/README.md
-echo -e "# Super Computo\n #### Estado:\n'''' Se ejecutaron $procesos procesos ''''"  > $fich
