128c271f88286c74c8101b29ba6790990a3ebe83
SkyllerXBruce <skyllerx@gmail.com>
2017-07-30 16:53:41 -0500

    modificacion

--- monitoreo.sh
+++ monitoreo.sh
@@ -5,6 +5,6 @@ FICH=/home/bruce/Escritorio/Lab_Super_Computo/Repositorios/README.md
 echo -e "# Super Computo\n####Estado:\n\n~~~~~\nSe ejecutaron $PROCESOS procesos\n~~~~~"  > $FICH
 
 git add *
-git commit -m "creación y modificacion"
-git remote add origin git@github.com:SkyllerXBruce/user.git
+git commit -m "modificacion"
 git push -u origin master
+cat monitor.sh
