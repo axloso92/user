241d8b66e64e6ee78d55159b99f57a4924ec05d5
SkyllerXBruce <skyllerx@gmail.com>
2017-07-30 16:57:06 -0500

    modificacion y actualización

--- monitoreo.sh
+++ monitoreo.sh
@@ -2,9 +2,9 @@
 
 PROCESOS=`ps -fea|wc -l`
 FICH=/home/bruce/Escritorio/Lab_Super_Computo/Repositorios/README.md
-echo -e "# Super Computo\n####Estado:\n\n~~~~~\nSe ejecutaron $PROCESOS procesos\n~~~~~"  > $FICH
+echo -e "# Super Computo\n####Estado:\n\n~~~~~\nProcesos en ejecución $PROCESOS\n~~~~~"  > $FICH
 
 git add *
-git commit -m "modificacion"
+git commit -m "modificacion y actualización"
 git push -u origin master
-cat monitoreo.sh
+echo "procesos $PROCESOS"
